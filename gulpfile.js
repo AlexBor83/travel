153414ff0ab2eb5b30f0f1ed6d964991351a6ad3
AlexBor83 <rabota_for_alex@mail.ru>
2021-07-11 21:03:09 +0400

    готов html desctop

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,10 @@ const del = require('del');
 const csso = require('gulp-csso');
 const jsmin = require('gulp-jsmin');
 const htmlmin = require('gulp-htmlmin');
+const ttf2woff = require("gulp-ttf2woff");
+const ttfwoff2 = require("gulp-ttf2woff2");
+
+let fs = require("fs");
 
 // Styles
 
@@ -53,6 +57,52 @@ const html = () => {
 }
 
 exports.html = html;
+
+const fonts = () => {
+  gulp.src("source/fonts/*.ttf")
+  .pipe(ttf2woff())
+  .pipe(gulp.dest("build/fonts/"));
+
+  return gulp.src("source/fonts/*.ttf")
+  .pipe(ttfwoff2())
+  .pipe(gulp.dest("build/fonts/"));
+}
+
+exports.fonts = fonts;
+
+
+// function fontsStyle() {
+//   let file_content = fs.readFileSync('source/sass/blocks/fonts.scss');
+//   if (file_content == "") {
+//     fs.writeFile('source/sass/blocks/fonts.scss', "", cb);
+//     return fs.readdir('build/fonts/', function (err, items) {
+//       if (items) {
+//         let c_fontname;
+//         for (var i = 0; i < items.length; i++) {
+//           let fontname = items[i].split(".");
+//           fontname = fontname[0];
+//           if (c_fontname != fontname) {
+//             fs.appendFile(
+//               'source/sass/blocks/fonts.scss',
+//               '@include font("' +
+//                 fontname +
+//                 '", "' +
+//                 fontname +
+//                 '", "400", "normal");\r\n',
+//               cb
+//             );
+//           }
+//           c_fontname = fontname;
+//         }
+//       }
+//     });
+//   }
+// }
+
+// function cb() {}
+// exports.fontsStyle = fontsStyle;
+
+
 
 const img = () => {
   return gulp.src("source/img/**/*.{jpg,png,svg}")
@@ -129,6 +179,7 @@ const build = gulp.series(
   clean,
   copy,
   html,
+  fonts,
   styles,
   stylesbig,
   imgTask,
